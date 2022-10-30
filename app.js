1c7f648245d3a8e3feac02e4c5a0c98d8eb49843
admin <admin@kvantus.tech>
2022-10-30 23:14:13 +0300

    refactor: 3.4 change tag selectors to class selectors in app.js index.html style.css

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput = document.getElementById("new-task");//Add a new task.
-var addButton = document.getElementsByTagName("button")[0];//first button
+var addButton = document.getElementsByClassName("add")[0];//first button
 var incompleteTaskHolder = document.getElementById("incompletetasks");//ul of #incompleteTasks
 var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
 
@@ -33,17 +33,20 @@ var createNewTaskElement = function (taskString) {
     var deleteButtonImg = document.createElement("img");//delete button image
 
     label.innerText = taskString;
-    label.className = "task";
+    label.className = "label_task";
 
     //Each elements, needs appending
+    listItem.className = "li";
     checkBox.type = "checkbox";
+    checkBox.className = "checkbox";
     editInput.type = "text";
-    editInput.className = "task";
+    editInput.className = "input_task";
 
     editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
     editButton.className = "edit";
 
     deleteButton.className = "delete";
+    deleteButtonImg.className = "delete__img";
     deleteButtonImg.src = "./remove.svg";
     deleteButton.appendChild(deleteButtonImg);
 
